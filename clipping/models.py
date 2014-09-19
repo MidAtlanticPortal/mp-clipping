66f9b6a51ffb4f089160faf29485403839499af4
Seth Hill <seth@pointnineseven.com>
2014-09-19 11:47:17 -0700

    Remove unused code MP-135

--- clipping/models.py
+++ clipping/models.py
@@ -6,17 +6,12 @@ from manipulators.models import BaseManipulatorGeometry
 
     
 class Marine(BaseManipulatorGeometry):
-    name = models.CharField(verbose_name="Marine Geometry Name", max_length=255, blank=True)
-    geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True, verbose_name="Marine Only")
+    name = models.CharField(verbose_name="Marine Geometry Name", max_length=255,
+                            blank=True)
+    geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, 
+                                        null=True, blank=True, 
+                                        verbose_name="Marine Only")
 
     def __unicode__(self):
         return "Marine Layer, created: %s" % (self.creation_date)
-
-'''
-# Used the following for loading shapefiles into postgres and then copied the geometries over from the shell command line    
-# marine = Marine(name='marine_only', geometry=marine_shp_geom)    
-       
-class Marine_Shp(models.Model):
-    geometry = models.MultiPolygonField(srid=settings.GEOMETRY_DB_SRID, null=True, blank=True)
-'''
 
